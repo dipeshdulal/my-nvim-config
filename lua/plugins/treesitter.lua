8b737fc9f69fb003bba2552c299f2afda36c5ea7
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2023-12-26 09:51:14 +0545

    fix: update treesitter not working in help files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     local configs = require("nvim-treesitter.configs")
 
@@ -8,7 +9,7 @@ return {
       ensure_installed = {
         "c", "lua", "javascript", "html",
         "go", "dart", "typescript", "python",
-        "dart"
+        "dart", "vimdoc", "vim",
       },
       highlight = { enable = true },
       indent = { enable = true },
