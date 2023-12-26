cabc4b1aa17f33e6804c05ba75a7303ff8841e5d
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2023-12-26 10:15:36 +0545

    feat: incremental seleciton enabled

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = {
-        "c", "lua", "javascript", "html",
-        "go", "dart", "typescript", "python",
-        "dart", "vimdoc", "vim",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+      configs.setup({
+        ensure_installed = {
+          "c", "lua", "javascript", "html",
+          "go", "dart", "typescript", "python",
+          "dart", "vimdoc", "vim",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<CR>',
+            node_incremental = '<CR>',
+            scope_incremental = '<CR>',
+            node_decremental = '<BS>'
+          }
+        }
+      })
+    end
+  },
 }
