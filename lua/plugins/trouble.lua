70931ee37513fe734107bbfa92ffd5afa5665d16
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2024-01-02 15:38:37 +0545

    feat: added trouble nvim

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require('trouble').setup()
+    vim.keymap.set("n", "<leader>t", "<cmd>TroubleToggle workspace_diagnostics<CR>")
+  end
+}
