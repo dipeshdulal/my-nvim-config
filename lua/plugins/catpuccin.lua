f3620faba02abf70bc234eb63b62eed296737806
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2024-02-12 11:30:19 +0545

    remove bg blurs, use catppuccin

---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local catpuccin = require("catppuccin")
+      catpuccin.setup({})
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme("catppuccin")
+    end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    lazy = false,
+    config = function()
+      local lualine = require("lualine")
+      lualine.setup({
+        options = {
+          theme = "kanagawa"
+        }
+      })
+    end
+  },
+}
