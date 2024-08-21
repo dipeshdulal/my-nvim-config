01ae0089d2e5f85414f554ec95fb67a9d3e7382c
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2024-08-21 12:54:13 +0545

    fix: trouble configuration

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,6 +3,6 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require('trouble').setup()
-    vim.keymap.set("n", "<leader>t", "<cmd>TroubleToggle workspace_diagnostics<CR>")
+    vim.keymap.set("n", "<leader>t", "<cmd>Trouble diagnostics toggle<CR>")
   end
 }
