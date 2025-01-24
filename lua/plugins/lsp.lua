ef9fcad0d3fb053ad8c5dd6070cf46835d7964f9
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2025-01-24 10:53:07 +0545

    feat: update configurations for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,14 +52,17 @@ return {
       { 'williamboman/mason-lspconfig.nvim' },
       {
         "nvimtools/none-ls.nvim",
+        dependencies = {
+          "nvimtools/none-ls-extras.nvim",
+        },
         config = function()
           local null_ls = require("null-ls")
           null_ls.setup({
             sources = {
-              null_ls.builtins.diagnostics.eslint,
               null_ls.builtins.formatting.prettier,
               null_ls.builtins.code_actions.gitsigns,
               null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+              require("none-ls.diagnostics.eslint"),
             },
           })
         end,
