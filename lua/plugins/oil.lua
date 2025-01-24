7d516697a9f83abf05555c9a7590c5f96a1b0524
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2025-01-24 10:56:59 +0545

    feat: show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   'stevearc/oil.nvim',
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true
+      }
+    })
 
     -- netrw shortcut
     vim.keymap.set("n", '<C-n>', '<esc><cmd>Oil<cr>')
