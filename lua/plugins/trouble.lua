56732f052b0ac0ce36571d693d09eebdafd6bebe
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2025-06-26 12:57:39 +0545

    feat: modify trouble nvim config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,7 +2,11 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require('trouble').setup()
+    require('trouble').setup({
+      auto_preview = false,
+      auto_open = false,
+      follow = false,
+    })
     vim.keymap.set("n", "<leader>t", "<cmd>Trouble diagnostics toggle<CR>")
   end
 }
