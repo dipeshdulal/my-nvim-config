7ba8702668ccd888ad7d56ae2d7f666081b12656
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2025-06-26 12:45:48 +0545

    fix: oil nvim overwriting the window switch keymap

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,10 @@ return {
     require("oil").setup({
       view_options = {
         show_hidden = true
+      },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
       }
     })
 
